26055cae10aa10f75665e7077616119a221a3e75
david elkabas <davidelkabas@davids-MacBook-Air.local>
2026-02-02 18:29:51 +0100

    Change banner title to ROBOPUNKS v2

--- src/App.tsx
+++ src/App.tsx
@@ -185,7 +185,7 @@ export default function App() {
         style={{ fontSize: 92, letterSpacing: 12 }}
         filter="url(#arcadeGlowGreen)"
       >
-        ROBOPUNKS
+        ROBOPUNKS V2 
       </text>
 
       {/* TOP RIGHT LABEL */}
